b3b64e01251e716c7bdad2b1e4ad86a9c7c6a054
Denis <andreev.denis0693@gmail.com>
2024-07-12 18:11:17 +0300

    7

--- src/components/SelectionMode.jsx
+++ src/components/SelectionMode.jsx
@@ -13,18 +13,20 @@ const SelectionMode = () => {
   const options = ["Option 1", "Option 2", "Option 3", "Option 4"];
 
   return (
-    <div className={styles.selectionMode}>
-      <div className={styles.question}>{question}</div>
-      <div className={styles.options}>
-        {options.map((option, index) => (
-          <button
-            key={index}
-            className={`${styles.option} ${selectedOption === option ? styles.selected : ''}`}
-            onClick={() => handleOptionClick(option)}
-          >
-            {option}
-          </button>
-        ))}
+    <div className={styles.wrapper}>
+      <div className={styles.selectionMode}>
+        <div className={styles.question}>{question}</div>
+        <div className={styles.options}>
+          {options.map((option, index) => (
+            <button
+              key={index}
+              className={`${styles.option} ${selectedOption === option ? styles.selected : ''}`}
+              onClick={() => handleOptionClick(option)}
+            >
+              {option}
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
