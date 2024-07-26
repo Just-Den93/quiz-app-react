3777ab3b0cbde6f10083d062a7568c5ebfa758ec
Denis Andreev <andreev.denis0693@gmail.com>
2024-07-26 16:54:12 +0300

    3

--- src/components/QuizCard.jsx
+++ src/components/QuizCard.jsx
@@ -1,20 +1,11 @@
-// src/components/QuizCard.jsx
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import styles from '../styles/QuizCard.module.css';
 
-function QuizCard({ showQuizPage }) {
-  const navigate = useNavigate();
-
-  const handleStartQuiz = () => {
-    showQuizPage();
-    navigate('/quiz');
-  };
-
+function QuizCard({ startQuiz }) {
   return (
     <div className={styles.container}>
       <h2>Quiz Card</h2>
-      <button onClick={handleStartQuiz}>Start Quiz</button>
+      <button onClick={startQuiz}>Почати наживо</button>
     </div>
   );
 }
