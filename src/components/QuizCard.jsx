6bdf759643345e09c14afd6757b73a2cac732a2f
Denis <andreev.denis0693@gmail.com>
2024-07-29 20:07:22 +0300

    6

--- src/components/QuizCard.jsx
+++ src/components/QuizCard.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import styles from '../styles/QuizCard.module.css';
+import QuizPage from './QuizPage';
 
-function QuizCard({ startQuiz }) {
+function QuizCard({ startQuiz, showMainMenu, handleNewGame }) {
   return (
-    <div className={styles.container}>
-      <h2>Quiz Card</h2>
-      <button onClick={startQuiz}>Почати наживо</button>
+    <div className={styles.card}>
+      <div className={styles.image}>
+        <QuizPage miniature={true} showMainMenu={showMainMenu} handleNewGame={handleNewGame} />
+      </div>
+      <div className={styles.details}>
+        <h2>Вікторина Південна</h2>
+        <p>20 запитань</p>
+        <p>Just_Dens</p>
+        <div className={styles.bottomRow}>
+          <p className={styles.games}>Ігри: 0</p>
+          <button className={styles.startButton} onClick={startQuiz}>
+            Провести наживо
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
