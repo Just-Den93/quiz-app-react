32577ee93987f81917568bc9071c1fe400679f5d
Denis <andreev.denis0693@gmail.com>
2024-08-04 22:16:22 +0300

    12

--- src/components/QuizPage.jsx
+++ src/components/QuizPage.jsx
@@ -7,46 +7,47 @@ import Settings from './Settings';
 import styles from '../styles/QuizPage.module.css';
 
 function QuizPage({ showMainMenu, handleNewGame, miniature = false }) {
-  const [usedBlocks, setUsedBlocks] = useState(() => {
-    const saved = localStorage.getItem('usedBlocks');
-    return saved ? JSON.parse(saved) : {};
-  });
-
-  const [isSettingsVisible, setIsSettingsVisible] = useState(false);
-
-  const markBlockAsUsed = (categoryName, blockId) => {
-    setUsedBlocks((prevUsedBlocks) => {
-      const updatedUsedBlocks = { ...prevUsedBlocks };
-      if (!updatedUsedBlocks[categoryName]) {
-        updatedUsedBlocks[categoryName] = [];
-      }
-      updatedUsedBlocks[categoryName].push(blockId);
-      localStorage.setItem('usedBlocks', JSON.stringify(updatedUsedBlocks));
-      return updatedUsedBlocks;
+    const [usedBlocks, setUsedBlocks] = useState(() => {
+        const saved = localStorage.getItem('usedBlocks');
+        return saved ? JSON.parse(saved) : {};
     });
-  };
 
-  useEffect(() => {
-    localStorage.setItem('usedBlocks', JSON.stringify(usedBlocks));
-  }, [usedBlocks]);
+    const [isSettingsVisible, setIsSettingsVisible] = useState(false);
 
-  const showSettings = () => {
-    setIsSettingsVisible(true);
-  };
+    const markBlockAsUsed = (categoryName, blockId) => {
+        setUsedBlocks((prevUsedBlocks) => {
+            const updatedUsedBlocks = { ...prevUsedBlocks };
+            if (!updatedUsedBlocks[categoryName]) {
+                updatedUsedBlocks[categoryName] = [];
+            }
+            updatedUsedBlocks[categoryName].push(blockId);
 
-  const hideSettings = () => {
-    setIsSettingsVisible(false);
-  };
+            localStorage.setItem('usedBlocks', JSON.stringify(updatedUsedBlocks));
+            return updatedUsedBlocks;
+        });
+    };
 
-  return (
-    <div className={`${styles.quiz_page} ${miniature ? styles.miniature : ''}`}>
-      <Header />
-      <ContentContainer usedBlocks={usedBlocks} markBlockAsUsed={markBlockAsUsed} />
-      <EndMessage />
-      <MenuModal showSettings={showSettings} showMainMenu={showMainMenu} />
-      {isSettingsVisible && <Settings onClose={hideSettings} />}
-    </div>
-  );
+    useEffect(() => {
+        localStorage.setItem('usedBlocks', JSON.stringify(usedBlocks));
+    }, [usedBlocks]);
+
+    const showSettings = () => {
+        setIsSettingsVisible(true);
+    };
+
+    const hideSettings = () => {
+        setIsSettingsVisible(false);
+    };
+
+    return (
+        <div className={`${styles.quiz_page} ${miniature ? styles.miniature : ''}`}>
+            <Header />
+            <ContentContainer usedBlocks={usedBlocks} markBlockAsUsed={markBlockAsUsed} />
+            <EndMessage />
+            <MenuModal showSettings={showSettings} showMainMenu={showMainMenu} />
+            {isSettingsVisible && <Settings onClose={hideSettings} />}
+        </div>
+    );
 }
 
 export default QuizPage;
