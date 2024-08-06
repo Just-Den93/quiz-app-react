ba01129ae35be1e91862193159b86fbbe051e47c
Denis Andreev <andreev.denis0693@gmail.com>
2024-08-06 09:23:59 +0300

    24

--- src/utils/loadData.js
+++ src/utils/loadData.js
@@ -1,11 +1,4 @@
 export function loadFileCount() {
-<<<<<<< HEAD
-	const context = require.context('../data', false, /\.js$/);
-	return context.keys().length;
- }
- 
-=======
   const context = require.context('../data', false, /\.json$/);
   return context.keys().length;
 }
->>>>>>> 03b1859d05dbccf6df34d2614aa7cf4a5ac3baf5
