e2de75db4a9aa66f34e760fb2242489343028dae
Denis Andreev <andreev.denis0693@gmail.com>
2024-08-15 17:13:53 +0300

    15.08.24/17:00

--- src/components/ContentContainer.jsx
+++ src/components/ContentContainer.jsx
@@ -1,36 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import CategoryRow from './CategoryRow';
-import Modal from './Modal';
-import { useQuizContext } from '../context/QuizContext';
 import styles from '../styles/ContentContainer.module.css';
 
-function ContentContainer({ data }) {
-  const [selectedBlock, setSelectedBlock] = useState(null);
-  const { currentQuizId, markBlockAsUsed, quizStates } = useQuizContext();
-  const usedBlocks = quizStates[currentQuizId]?.usedBlocks || {};
-
-  const handleItemClick = (block, categoryId) => {
-    console.log(`handleItemClick called for block ID: ${block.id}, category ID: ${categoryId}`);
-    setSelectedBlock(block);
-    markBlockAsUsed(currentQuizId, categoryId, block.id);
-  };
-
+function ContentContainer({ data, onBlockSelect }) {
   return (
     <div id="content-container" className={styles.contentContainer}>
       {data.map((category) => (
         <CategoryRow
           key={category.id}
           category={category}
-          usedBlocks={usedBlocks}
-          onItemClick={handleItemClick}
+          onBlockSelect={onBlockSelect}
         />
       ))}
-      {selectedBlock && (
-        <Modal
-          block={selectedBlock}
-          onClose={() => setSelectedBlock(null)}
-        />
-      )}
     </div>
   );
 }
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import styles from '../styles/Item.module.css';
 
-function Item({ block, onClick, used }) {
-  console.log(`Rendering Item with block ID: ${block.id}, used: ${used}`);
+function Item({ block, categoryId, onBlockSelect }) {
+  const handleClick = () => {
+    console.log(`Item clicked: Block ID ${block.id}, Category ID ${categoryId}`); // Отладочное сообщение
+    onBlockSelect({ ...block, categoryId }); // Передаем информацию о блоке наверх
+  };
 
   return (
     <button
-      className={`${styles.item} ${used ? styles.used : ''}`}
-      onClick={() => {
-        console.log(`Item clicked with block ID: ${block.id}`);
-        onClick(block);
-      }}
+      className={styles.item}
+      onClick={handleClick}
     >
       {block.id + 1}
     </button>
