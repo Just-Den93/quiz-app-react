986c1dfb81f8a8bc1bec1b9f1906030f7a703e0c
Denis <andreev.denis0693@gmail.com>
2024-08-19 01:17:21 +0300

    19.08.24/01:00

--- src/components/StartTimerButton/StartTimerButton.jsx
+++ src/components/StartTimerButton/StartTimerButton.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { LuTimer } from 'react-icons/lu'; // Импортируем иконку
+import { LuTimer } from 'react-icons/lu';
 import styles from './StartTimerButton.module.css';
 
 function StartTimerButton({ onClick }) {
   return (
     <button className={styles.startTimerButton} onClick={onClick}>
-      <LuTimer className={styles.startButtonIcon} /> {/* Используем иконку вместо изображения */}
+      <LuTimer className={styles.startButtonIcon} />
     </button>
   );
 }
